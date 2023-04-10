4ed3024b4064801f87d804d91dc03f5b6662e4f0
EnzoAkiraInoue <enzoakirainoue@gmail.com>
2023-04-10 19:05:05 -0300

    Calculadora changes static classes

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -5,6 +5,26 @@ public class CalculadoraTest {
 
     private Calculadora calculadora;
 
+    private static double valorPadrao1;
+    private static double valorPadrao2;
+
+    private static double valorRep4;
+
+    private static double valorRep3;
+
+    private static double valorRep2;
+
+    private static double valorRep1;
+    @BeforeAll
+    public static void definirValoresPadrao(){
+        valorPadrao1 = 10;
+        valorPadrao2 = 5;
+        valorRep1 = 20;
+        valorRep2 = 0;
+        valorRep3 = 100;
+        valorRep4 = 2;
+    }
+
     @BeforeEach
     public void iniciarCalculadora(){
         calculadora = new Calculadora();
@@ -13,28 +33,28 @@ public class CalculadoraTest {
     @Test
     @DisplayName("Soma funcional")
     public void Somar(){
-        calculadora.somar(10, 10);
-        Assertions.assertEquals(20, calculadora.getResultado());
+        calculadora.somar(valorPadrao1, valorPadrao1);
+        Assertions.assertEquals(valorRep1, calculadora.getResultado());
     }
 
     @Test
     @DisplayName("Subtração funcional")
     public void Subtrair(){
-        calculadora.subtrair(10, 10);
-        Assertions.assertEquals(0, calculadora.getResultado());
+        calculadora.subtrair(valorPadrao1, valorPadrao1);
+        Assertions.assertEquals(valorRep2, calculadora.getResultado());
     }
 
     @Test
     @DisplayName("multiplicação funcional")
     public void Multiplicar(){
-        calculadora.multiplicar(10, 10);
-        Assertions.assertEquals(100, calculadora.getResultado());
+        calculadora.multiplicar(valorPadrao1, valorPadrao1);
+        Assertions.assertEquals(valorRep3, calculadora.getResultado());
     }
 
     @Test
     @DisplayName("divisão funcional")
     public void dividir(){
-        calculadora.dividir(10, 5);
-        Assertions.assertEquals(2, calculadora.getResultado());
+        calculadora.dividir(valorPadrao1, valorPadrao2);
+        Assertions.assertEquals(valorRep4, calculadora.getResultado());
     }
 }
