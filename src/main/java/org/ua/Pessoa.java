852e808350414880f3b3115d96c577ac3b400594
Allyson <jandreyallyson@gmail.com>
2023-04-13 20:31:43 -0300

    Finalização da atividade 02

--- src/main/java/org/ua/Pessoa.java
+++ src/main/java/org/ua/Pessoa.java
@@ -51,7 +51,7 @@ public class Pessoa {
 
     private String nome;
 
-    private int idade;
+    private Integer idade;
 
     private String endereco;
 
@@ -59,22 +59,40 @@ public class Pessoa {
 
     private String telefone;
 
-    private int tempoDeTrabalho;
+    private Integer tempoDeTrabalho;
+
+    private Integer tempoParaSeAposentar;
 
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
-        this.nome = nome;
+        if (nome == null) {
+            throw new RuntimeException("O nome inserido é nulo");
+        } else if (nome.isEmpty()) {
+            throw new RuntimeException("O nome inserido está vazio");
+        } else if (nome.trim().length() < 3) {
+            throw new RuntimeException("O nome inserido tem menos do que 3 letras");
+        } else {
+            this.nome = nome;
+        }
     }
 
-    public int getIdade() {
+    public Integer getIdade() {
         return idade;
     }
 
-    public void setIdade(int idade) {
-        this.idade = idade;
+    public void setIdade(Integer idade) {
+        if (idade == null) {
+            throw new RuntimeException("O campo de idade está nulo");
+        } else if (idade > 120) {
+            throw new RuntimeException(("Não existe nenhuma pessoa viva com essa idade"));
+        } else if (idade < 0) {
+            throw new RuntimeException(("A idade não pode ser menor do que 0"));
+        } else {
+            this.idade = idade;
+        }
     }
 
     public String getEndereco() {
@@ -82,6 +100,13 @@ public class Pessoa {
     }
 
     public void setEndereco(String endereco) {
+        if (endereco == null) {
+            throw new RuntimeException("O campo de endereço está nulo");
+        } else if (endereco.isEmpty()) {
+            throw new RuntimeException("O campo de endereço está vazio");
+        } else if (endereco.trim().length() < 5) {
+            throw new RuntimeException("O endereço fornecido é muito curto");
+        }
         this.endereco = endereco;
     }
 
@@ -90,7 +115,13 @@ public class Pessoa {
     }
 
     public void setGenero(String genero) {
-        this.genero = genero;
+        if (genero == null) {
+            throw new RuntimeException("O campo de gênero está nulo");
+        } else if (genero.isEmpty()) {
+            throw new RuntimeException("O campo de gênero está vazio");
+        } else {
+            this.genero = genero;
+        }
     }
 
     public String getTelefone() {
@@ -98,15 +129,29 @@ public class Pessoa {
     }
 
     public void setTelefone(String telefone) {
-        this.telefone = telefone;
+        if (telefone == null) {
+            throw new RuntimeException("O campo de telefone está nulo");
+        } else if (telefone.isEmpty()) {
+            throw new RuntimeException("O campo de telefone está vazio");
+        } else if (telefone.trim().length() < 12) {
+            throw new RuntimeException("O telefone dado é inválido");
+        } else {
+            this.telefone = telefone;
+        }
     }
 
-    public int getTempoDeTrabalho() {
+    public Integer getTempoDeTrabalho() {
         return tempoDeTrabalho;
     }
 
-    public void setTempoDeTrabalho(int tempoDeTrabalho) {
-        this.tempoDeTrabalho = tempoDeTrabalho;
+    public void setTempoDeTrabalho(Integer tempoDeTrabalho) {
+        if (tempoDeTrabalho == null) {
+            throw new RuntimeException("O campo de tempo de trabalho está nulo");
+        } else if (tempoDeTrabalho < 0) {
+            throw new RuntimeException("O campo tempo de trabalho não pode ser menor que 0");
+        } else {
+            this.tempoDeTrabalho = tempoDeTrabalho;
+        }
     }
 
     public String getNomeETelefone() {
@@ -114,15 +159,60 @@ public class Pessoa {
     }
 
     public void setNomeETelefone(String nome, String telefone) {
-        this.nome = nome;
-        this.tempoDeTrabalho = tempoDeTrabalho;
+        if (nome == null || telefone == null) {
+            throw new RuntimeException("O campo de nome ou telefone está nulo");
+        } else if (nome.isEmpty() || telefone.isEmpty()) {
+            throw new RuntimeException("O campo de nome ou telefone está vazio");
+        } else {
+            this.nome = nome;
+            this.telefone = telefone;
+        }
     }
 
     public String getDadosCompletos() {
         return nome + idade + endereco + genero + telefone + tempoDeTrabalho;
     }
 
-    public void setDadosCompletos(String nome, int idade, String endereco, String telefone, int tempoDeTrabalho) {
+    public void setDadosCompletos(String nome, Integer idade, String endereco, String telefone, Integer tempoDeTrabalho) {
 
+        if (nome == null || idade == null || endereco == null || genero == null || telefone == null || tempoDeTrabalho == null) {
+            throw new RuntimeException("Um dos campos requeridos está nulo");
+        } else if (nome.isEmpty() || idade < 0 || endereco.isEmpty() || telefone.isEmpty() || tempoDeTrabalho < 0) {
+            throw new RuntimeException("Um dos campos requeridos está vazio ou com valor inválido");
+        } else {
+            this.nome = nome;
+            this.idade = idade;
+            this.endereco = endereco;
+            this.telefone = telefone;
+            this.tempoDeTrabalho = tempoDeTrabalho;
+        }
+    }
+
+    public int getTempoParaSeAposentar() {
+
+        if (genero.equals("Masculino")) {
+            return 65 - idade;
+        } else if (genero.equals("Feminino")) {
+            return 60 - idade;
+        } else {
+            return tempoParaSeAposentar;
+        }
+    }
+
+    public void setTempoParaSeAposentar(String genero, Integer idade) {
+
+        if (genero == "Masculino") {
+            if (idade >= 65) {
+                throw new RuntimeException("Um homem com idade maior que 65 já deve está aposentado");
+            } else if (idade < 65) {
+                this.tempoParaSeAposentar = 65 - idade;
+            }
+        } else if (genero == "Feminino") {
+            if (idade >= 60) {
+                throw new RuntimeException("Uma mulher com idade maior que 60 já deve está aposentada");
+            } else if (idade < 60) {
+                this.tempoParaSeAposentar = 60 - idade;
+            }
+        }
     }
 }
--- src/test/java/PessoaTest.java
+++ src/test/java/PessoaTest.java
@@ -1,5 +1,4 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 import org.ua.Pessoa;
 
 public class PessoaTest {
@@ -7,7 +6,7 @@ public class PessoaTest {
     private Pessoa pessoa;
     private static String nome;
 
-    private static int idade;
+    private static Integer idade;
 
     private static String endereco;
 
@@ -17,18 +16,210 @@ public class PessoaTest {
 
     private static int tempoDeTrabalho;
 
+    private static String nomeETelefone;
+
+    private static String dadosCompletos;
+
     @BeforeAll
     public static void definirValoresPadrao() {
         nome = "Allyson";
-        idade = 20;
+        idade = 50;
         endereco = "Foz do Iguaçu";
         genero = "Masculino";
-        telefone = "4512345678";
-        tempoDeTrabalho = 1;
+        telefone = "(45)12345-6789";
+        tempoDeTrabalho = 20;
+        nomeETelefone = nome + telefone;
+        dadosCompletos = nome + idade + endereco + genero + telefone + tempoDeTrabalho;
     }
 
     @BeforeEach
     public void inicializarPessoa() {
         pessoa = new Pessoa(nome, idade, endereco, genero, telefone, tempoDeTrabalho);
+    }
+
+    @Test
+    @DisplayName("Deve retornar o nome")
+    public void deveRetornarONome() {
+        Assertions.assertEquals(nome, pessoa.getNome());
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de nome nulo")
+    public void naoDeveReceberNomeNulo() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setNome(null));
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de nome vazio")
+    public void naoDeveReceberNomeVazio() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setNome(""));
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de nome com menos de 3 letras")
+    public void naoDeveReceberNomeComMenosDeTresLetras() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setNome("Al"));
+    }
+
+    @Test
+    @DisplayName("Deve retornar a idade")
+    public void deveRetornaraIdade() {
+        Assertions.assertEquals(idade, pessoa.getIdade());
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de idade nulo")
+    public void naoDeveReceberIdadeNulo() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setIdade(null));
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de idade maior que 120 anos")
+    public void naoDeveReceberIdadeMaiorQueLimite() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setIdade(121));
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de idade menor que 0")
+    public void naoDeveReceberIdadeMenorQueZero() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setIdade(-1));
+    }
+
+    @Test
+    @DisplayName("Deve retornar o endereço")
+    public void deveRetornarOEndereco() {
+        Assertions.assertEquals(endereco, pessoa.getEndereco());
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de endereço nulo")
+    public void naoDeveReceberEnderecoNulo() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setEndereco(null));
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de endereço vazio")
+    public void naoDeveReceberEnderecoVazio() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setEndereco(""));
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de endereço menor que 5 caracteres")
+    public void naoDeveReceberEnderecoMenorQueCinco() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setEndereco(null));
+    }
+
+    @Test
+    @DisplayName("Deve retornar o gênero")
+    public void deveRetornarOGenero() {
+        Assertions.assertEquals(genero, pessoa.getGenero());
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de gênero nulo")
+    public void naoDeveReceberGeneroNulo() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setGenero(null));
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de gênero vazio")
+    public void naoDeveReceberGeneroVazio() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setGenero(""));
+    }
+
+    @Test
+    @DisplayName("Deve retornar o telefone")
+    public void deveRetornarOTelefone() {
+        Assertions.assertEquals(telefone, pessoa.getTelefone());
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de telefone nulo")
+    public void naoDeveReceberTelefoneNulo() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setTelefone(null));
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de telefone vazio")
+    public void naoDeveReceberTelefoneVazio() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setTelefone(""));
+    }
+
+    @Test
+    @DisplayName("Não deve receber valor de telefone menor que 12 caracteres")
+    public void naoDeveReceberTelefoneMenorQueDezCaracteres() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setTelefone(""));
+    }
+
+    @Test
+    @DisplayName("Deve retornar o tempo de trabalho")
+    public void deveRetornarOTempoDeTrabalho() {
+        Assertions.assertEquals(tempoDeTrabalho, pessoa.getTempoDeTrabalho());
+    }
+
+    @Test
+    @DisplayName("Não deve receber o valor de tempo de trabalho nulo")
+    public void naoDeveReceberOTempoDeTrabalhoNulo() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setTempoDeTrabalho(null));
+    }
+
+    @Test
+    @DisplayName("Não deve receber o valor de tempo de trabalho menor que 0")
+    public void naoDeveReceberOTempoDeTrabalhoMenorQueZero() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setTempoDeTrabalho(-1));
+    }
+
+    @Test
+    @DisplayName("Deve retornar o nome e telefone")
+    public void deveRetornarONomeETelefone() {
+        Assertions.assertEquals(nomeETelefone, pessoa.getNomeETelefone());
+    }
+
+    @Test
+    @DisplayName("Não deve receber o valor de nome e telefone nulo")
+    public void naoDeveReceberONomeETelefoneNulo() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setNomeETelefone(null, null));
+    }
+
+    @Test
+    @DisplayName("Não deve receber o valor de nome e telefone vazio")
+    public void naoDeveReceberONomeETelefoneVazio() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setNomeETelefone("", ""));
+    }
+
+    @Test
+    @DisplayName("Deve retornar os dados completos")
+    public void deveRetornarOsDadosCompletos() {
+        Assertions.assertEquals(dadosCompletos, pessoa.getDadosCompletos());
+    }
+
+    @Test
+    @DisplayName("Não deve receber um dos campos dos dados completos nulo")
+    public void naoDeveReceberDadosCompletosNulo() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setDadosCompletos(null, null, null, null, null));
+    }
+
+    @Test
+    @DisplayName("Não deve receber um dos campos dos dados completos vazio")
+    public void naoDeveReceberDadosCompletosVazio() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setDadosCompletos("", -1, "", "", -1));
+    }
+
+    @Test
+    @DisplayName("Deve retornar o tempo para se aposentar")
+    public void deveRetornarOTempoParaSeAposentar() {
+        Assertions.assertEquals(15, pessoa.getTempoParaSeAposentar());
+    }
+
+    @Test
+    @DisplayName("Não deve receber tempo de trabalho maior que 65 (Masculino)")
+    public void naoDeveReceberIdadeMaiorQueAposentarMasculino() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setTempoParaSeAposentar("Masculino", 70));
+    }
+
+    @Test
+    @DisplayName("Não deve receber tempo de trabalho maior que 60 (Feminino)")
+    public void naoDeveReceberIdadeMaiorQueAposentarFeminino() {
+        Assertions.assertThrows(RuntimeException.class, () -> pessoa.setTempoParaSeAposentar("Feminino", 65));
     }
 }
