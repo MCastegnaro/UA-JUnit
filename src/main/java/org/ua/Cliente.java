3638306ee7383177692b416a5ea3c0a0529b9df4
Castegnaro <matheuscastegnaro@gmail.com>
2023-04-13 20:19:15 -0300

    Criando a classe de cliente

---
+++ src/main/java/org/ua/Cliente.java
@@ -0,0 +1,13 @@
+package org.ua;
+
+public class Cliente {
+    private String name;
+
+    public Cliente(String name){
+        this.name  = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
