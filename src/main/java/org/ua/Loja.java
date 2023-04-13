0b19c7f88d2a31c7592bb8a89388c0c85646e496
Castegnaro <matheuscastegnaro@gmail.com>
2023-04-13 20:24:24 -0300

    Adicionando classe de loja

---
+++ src/main/java/org/ua/Loja.java
@@ -0,0 +1,24 @@
+package org.ua;
+
+import java.util.List;
+import java.util.Optional;
+
+public class Loja {
+    private List<Cliente> clientes;
+
+    public void adicionarClientes(Cliente cliente){
+        clientes.add(cliente);
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public boolean removerCliente(Cliente cliente){
+        return clientes.remove(cliente);
+    }
+
+    public Optional<Cliente> buscarClientePorNome(String nome) {
+        return clientes.stream().filter(cliente -> cliente.getName().equals(nome)).findFirst();
+    }
+}
